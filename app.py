102b84adf3c9834ffaf7cabda6e19af8fe9ee192
Алена Раева <alena.kra15@gmail.com>
2024-08-30 10:45:20 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, Response
+from prometheus_client import Gauge, generate_latest
 
 app = Flask(__name__)
 
 todos = []
 
+# Создайте метрику для количества запросов
+requests_total = Gauge('my_website_requests_total', 'Total number of requests to my website')
+
+# Создайте метрику для количества добавленных задач
+todos_added_total = Gauge('my_website_todos_added_total', 'Total number of todos added')
+
 @app.route('/')
 def index():
+    requests_total.inc()
     return render_template('index.html', todos=todos)
 
 @app.route('/add', methods=['POST'])
@@ -13,6 +21,7 @@ def add_todo():
     todo = request.form.get('todo')
     if todo:
         todos.append(todo)
+        todos_added_total.inc()
     return redirect('/')
 
 @app.route('/delete/<int:todo_id>')
@@ -21,5 +30,10 @@ def delete_todo(todo_id):
         todos.pop(todo_id)
     return redirect('/')
 
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype="text/plain")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
